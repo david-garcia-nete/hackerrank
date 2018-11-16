3e3b4cae45ca38cd8b9c2c0d7b0b4c13691de0b7
David Garcia <david.garcia.nete@gmail.com>
2018-11-16 01:02:19 -0500

    Create counting valleys test.

---
+++ src/Practice/InterviewPreparationKit/WarmUpChallenges/CountingValleys.php
@@ -0,0 +1,40 @@
+<?php
+
+final class CountingValleys
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function CountingValleys($n, $s): int
+    {
+
+        $meter = 0;
+
+        $valleyCount = 0;
+
+        foreach ($s as $step) {
+
+            if ($step == 'U') {
+
+                if($meter === -1) $valleyCount++;
+
+                $meter++;
+
+            }
+
+            if ($step == 'D') {
+
+                $meter--;
+
+            }
+
+        }
+
+        return $valleyCount;
+
+    }
+
+}
---
+++ tests/Practice/InterviewPreparationKit/WarmUpChallenges/CountingValleysTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+include '/home/david/PhpstormProjects/hackerrank/src/Practice/InterviewPreparationKit/WarmUpChallenges/CountingValleys.php';
+
+final class CountingValleysTest extends TestCase
+{
+
+    public function testCountingValleys(): void
+    {
+        $countingValleys = new CountingValleys();
+
+        $n = 20;
+        $s = "UDDUUUDDUUDDDUUUUUUD";
+
+        $this->assertEquals(
+            2,
+            $countingValleys->countingValleys($n, $s)
+        );
+
+        $n = 26;
+        $s = "UDDDUUUUUUDDDUUUUUUDDDUUUUU";
+
+        $this->assertEquals(
+            1,
+            $countingValleys->countingValleys($n, $s)
+        );
+
+    }
+
+}
